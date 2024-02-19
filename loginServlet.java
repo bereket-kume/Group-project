2fa2ecf4921265b81c3ca5e759dfe90f84d09af0
Segni gutu <136971056+segnigutu@users.noreply.github.com>
2024-02-19 22:46:17 +0300

    changed
    carefull

--- loginServlet.java
+++ loginServlet.java
@@ -1,4 +1,4 @@
-import jakarta.servlet.RequestDispatcher;
+package OnlineShop;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -7,33 +7,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-@WebServlet(urlPatterns = {"/loginServlet"})
+@WebServlet("/loginServlet")
 public class loginServlet extends HttpServlet {
   @Override 
      protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException{
-        String customerId=request.getParameter("id");
+        String email=request.getParameter("email");
         String password=request.getParameter("password");
-        DatabaseClass d_class;
+        
+       DatabaseClass d_class = null;
       try {
           d_class = new DatabaseClass();
-           if (d_class.isExist(customerId,password,"customerTable")){
-               RequestDispatcher redi=request.getRequestDispatcher("homePage.html");
-               redi.forward(request, response);
-          
-            
-        }
-      } catch (Exception ex) {
-          Logger.getLogger(loginServlet.class.getName()).log(Level.SEVERE, null, ex);
-      }
-       
-        
+    if (d_class.isExist(email, password)) {
+        response.sendRedirect("homepage.html");
+    } 
+    else {
+        response.sendRedirect("index.html");
     }
-    @Override 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request,response);
-    
-        
-    }
+} catch (Exception ex) {
+    Logger.getLogger(loginServlet.class.getName()).log(Level.SEVERE, null, ex);
+}
+}
 }
